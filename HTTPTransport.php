12a104b338c36fa0d6da46a0d357446264105812
Aaron Shafovaloff <aaronshaf@gmail.com>
2012-04-18 17:04:38 -0600

    Initial commit

---
+++ HTTPTransport.php
@@ -0,0 +1,51 @@
+<?php
+//http://pear.php.net/manual/en/package.http.http-request2.php
+
+require_once "HTTP/Request2.php";
+
+if (!defined('CURL_OPERATION_TIMEDOUT'))
+    define('CURL_OPERATION_TIMEDOUT', 28);
+
+class HTTPTransport {
+    const TIMEOUT = 5;
+    protected $ch;
+	
+    public function __construct($host='localhost', $port=80) {
+        $this->host = $host;
+        $this->port = $port;
+        $this->ch = curl_init();
+    }
+
+    public function request($url, $method="GET", $payload=false) {
+        $method = strtoupper($method);
+        $protocol = "http";
+        $requestURL = $protocol . "://" . $this->host . ':' . $this->port . $url;
+			
+    	$request = new HTTP_Request2($requestURL);
+		$request->setMethod($method);
+		
+		if($method === 'POST') {
+			if(!empty($payload) ) {
+				if(is_array($payload)) {
+					$payload = json_encode($payload);
+				}
+			}
+			$request->setBody($payload);
+		}
+		
+		try {
+		    $response = $request->send();
+		    if (200 == $response->getStatus()) {
+		        $body = $response->getBody();
+		    } else {
+		        echo 'Unexpected HTTP status: ' . $response->getStatus() . ' ' .
+		             $response->getReasonPhrase() . '<br/>';
+				echo $response->getBody();
+		    }
+		} catch (HTTP_Request2_Exception $e) {
+		    echo 'Error: ' . $e->getMessage();
+		}
+		
+        return $body;
+    }
+}
